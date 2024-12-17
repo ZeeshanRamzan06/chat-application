aab3c248062aa98000af45d7cba6df706e53562c
ZeeshanRamzan06 <zeeshanramzan3f@gmail.com>
2024-12-18 00:01:33 +0500

    message api

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,3 +1,4 @@
+import cloudinary from "../lib/cloudinary.js";
 import { generateToken } from "../lib/utils.js";
 import User from "../models/user.js";
 import bcrypt from 'bcryptjs'
@@ -74,6 +75,35 @@ export const logout = (req,res) => {
         res.status(200).json({message:"Logout successfully"})
     } catch (error) {
         console.log('Error in logout Controlerl',error.message)
+        res.status(500).json({message:"Internal Server Error"})
+    }
+}
+
+export const updateProfile  = async (req,res) =>{
+    try {
+        const {profilePic} = req.body;
+        const userId =req.user._id;
+
+        if(!profilePic){
+            res.status(400).json({message:"Profile pic is required"})
+        }
+
+        const uploadResponse = await cloudinary.uploader.upload(profilePic)
+        const updateUser = await User.findByIdAndUpdate(userId, {profilePic:uploadResponse.secure_url},{new:true})
+
+        res.status(200).json(updateUser)
+    } catch (error) {
+        console.log("error in update profile:" , error.message)
+
+        res.status(500).json({message:"Internal Server Error"})
+    }
+}
+
+export const checkAuth = (req,res) =>{
+    try {
+        res.status(200).json(req.user)
+    } catch (error) {
+        console.log("Error in checkAuth Controller",error.message)
         res.status(500).json({message:"Internal Server Error"})
     }
 }
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,17 @@
 import express, { json } from "express";
-import authRoutes from './routes/auth.route.js'
 import dotenv from 'dotenv'
 import { connectDb } from "./lib/db.js";
+import cookieParser from 'cookie-parser';
+import authRoutes from './routes/auth.route.js'
+import messageRoutes from './routes/message.route.js'
 dotenv.config();
 const app = express();
-app.use(json())
+
+
+app.use(express.json())
+app.use(cookieParser())
 app.use('/api/auth',authRoutes )
+app.use('/api/message', messageRoutes )
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, ()=>{
