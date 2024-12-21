d1f5dd0d00b5600660a9ec86bb67e59e56d2e801
ZeeshanRamzan06 <zeeshanramzan3f@gmail.com>
2024-12-22 01:17:11 +0500

    completed all

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,24 +6,34 @@ import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import cors from "cors";
 import { app, server } from "./lib/socket.js";
+import path from "path";
 
 dotenv.config();
 
+const __dirname = path.resolve();
 // Middleware
 const corsOptions = {
   origin: "http://localhost:5173", // Allow your frontend origin
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true, // Allow credentials (cookies, etc.)
 };
-app.use(cors(corsOptions)); // CORS middleware
-app.use(express.json({ limit: "10mb" })); // JSON body parser with payload limit
-app.use(express.urlencoded({ limit: "10mb", extended: true })); // URL-encoded parser
+app.use(cors(corsOptions)); 
+app.use(express.json({ limit: "10mb" })); 
+app.use(express.urlencoded({ limit: "10mb", extended: true }));
 app.use(cookieParser()); // Cookie parser
 
 // Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 // Start Server
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
