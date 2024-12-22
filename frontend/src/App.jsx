23ce86e7c0b08251ae062a31e5c4a261b6bc6578
ZeeshanRamzan06 <zeeshanramzan3f@gmail.com>
2024-12-22 20:43:54 +0500

    fix errors

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,9 +7,9 @@ import SettingPage from './pages/SettingPage'
 import ProfilePage from './pages/ProfilePage.jsx'
 import { Loader } from 'lucide-react'
 import {Toaster} from 'react-hot-toast'
-import LoginPage from './pages/LogInPage.jsx'
 import { useThemeStore } from './store/useThemeStore.js'
 import useAuthStore from './store/useAuthStore.js';
+import LogInPage from './pages/LogInPage.jsx'
 
 const App = () => {
   const {authUser , checkAuth, isCheckingAuth} = useAuthStore();
@@ -32,7 +32,7 @@ const App = () => {
     <Routes>
       <Route path="/" element={authUser ? <HomePage /> : <Navigate to="/login" />} />
       <Route path="/signup" element={!authUser ? <SignUpPage /> : <Navigate to="/" />} />
-      <Route path="/login" element={!authUser ? <LoginPage /> : <Navigate to="/" />} />
+      <Route path="/login" element={!authUser ? <LogInPage /> : <Navigate to="/" />} />
       <Route path="/settings" element={<SettingPage />} />
       <Route path="/profile" element={authUser ? <ProfilePage /> : <Navigate to="/login" />} />
     </Routes>
